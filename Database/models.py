4bf9c5ca346328364b41950500c1d9e2a625f257
Matteo <matteo.liera@gmail.com>
2017-08-21 17:32:31 +0200

    Initial commit

---
+++ Database/models.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+from sqlalchemy import Column, Integer, String, DateTime
+from Database.database import Base
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique=False)
+    email = Column(String(120), unique=True)
+    address = Column(String(120), unique=False)
+    phone = Column(String(12), unique=False)
+    qualification = Column(String(120), unique=False)
+    registration = Column(String(120), unique=False)
+    registration_date = Column(DateTime, unique=False)
+    confirmed = Column(Integer, unique=False)
+
+    def __init__(self, name=None, email=None, address=None, phone=None, qualification=None, registration=None, registrationDate = None):
+        self.name = name
+        self.email = email
+        self.address = address
+        self.phone = phone
+        self.qualification = qualification
+        self.registration = registration
+        if registrationDate is None:
+            self.registrationDate = datetime.utcnow()
+
+    def __repr__(self):
+        return '<User %r>' % self.name
+
---
+++ Server/server.py
@@ -0,0 +1,35 @@
+# -*- coding: UTF-8 -*-
+"""
+hello_flask: First Python-Flask webapp
+"""
+from flask import Flask, jsonify, render_template  # From module flask import class Flask
+from flask_oauthlib.provider import OAuth2Provider
+from Database.database import db_session, init_db
+
+app = Flask(__name__)  # Construct an instance of Flask class for our webapp
+oauth = OAuth2Provider(app)
+
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
+
+
+@app.route('/')
+def main():
+    """Render an HTML template and return"""
+    return render_template('hello.html')  # HTML file to be placed under sub-directory templates
+
+
+if __name__ == '__main__':  # Script executed directly?
+    init_db()
+    app.run(debug=True, host='0.0.0.0')  # Launch built-in web server and run this Flask webapp
+
+
+@app.route('/a')
+def a():
+    return jsonify({'message': 'unknown user'}), 400
+    # response.data is:
+    # {
+    #   "message": "unknown user"
+    # }
