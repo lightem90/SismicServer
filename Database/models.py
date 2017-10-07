ecaf1680bc43bb757fe56c0dfa843374d74ed6e3
Matteo <matteo.liera@gmail.com>
2017-10-07 13:24:19 +0200

    wip

--- Database/models.py
+++ Database/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from sqlalchemy import Column, Integer, String, DateTime
 from Database.database import Base
+from Crypto.Cipher import AES
 
 
 class User(Base):
@@ -8,29 +9,39 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(50), unique=False)
     email = Column(String(120), unique=True)
+    secret = Column(String(128), unique=True)
     address = Column(String(120), unique=False)
-    phone = Column(String(12), unique=False)
+    phone = Column(String(15), unique=False)
     qualification = Column(String(120), unique=False)
     registration = Column(String(120), unique=False)
     registration_date = Column(DateTime, unique=False)
     confirmed = Column(Integer, unique=False)
 
-    def __init__(self, name=None, email=None, address=None, phone=None, qualification=None, registration=None, registrationDate = None):
-        self.name = name
+    def __init__(self, email, password, name=None, address=None, phone=None, qualification=None, registration=None,
+                 registrationdate=None):
         self.email = email
+        self.password = password
+        self.name = name
         self.address = address
         self.phone = phone
         self.qualification = qualification
         self.registration = registration
-        if registrationDate is None:
+        if registrationdate is None:
             self.registrationDate = datetime.utcnow()
 
     def __repr__(self):
         return '<User %r>' % self.name
+
+    def hash_password(self, password):
+        obj = AES.new(self.name, AES.MODE_CBC, self.email)
+        self.secret = obj.encrypt(password)
+
+    def verify_password(self, password):
+        obj = AES.new(self.name, AES.MODE_CBC, self.email)
+        return password == obj.decrypt(self.secret)
 
 
 class SismicLocation(Base):
     __tablename__ = 'InformazioniLocazioneSismica'
     id = Column(Integer, primary_key=True)
     report_id = Column(Integer, unique=False)
-
--- Server/server.py
+++ Server/server.py
@@ -1,10 +1,10 @@
 # -*- coding: UTF-8 -*-
-"""
-hello_flask: First Python-Flask webapp
-"""
-from flask import Flask, jsonify, render_template  # From module flask import class Flask
-from Database.database import db_session, init_db
+from os import abort
 
+from flask import Flask, jsonify, render_template, request, url_for  # From module flask import class Flask
+
+from Database.database import db_session, init_db
+from Database.models import User
 
 app = Flask(__name__)  # Construct an instance of Flask class for our webapp
 
@@ -22,13 +22,54 @@ def main():
 
 if __name__ == '__main__':  # Script executed directly?
     init_db()
-    app.run(debug=True, host='0.0.0.0', port=5000)  # Launch built-in web server and run this Flask webapp
+    app.run(debug=True, host='192.168.0.2', port=5000)  # Launch built-in web server and run this Flask webapp
 
 
-@app.route('/')
-def a():
-    return jsonify({'message': 'unknown user'}), 400
-    # response.data is:
-    # {
-    #   "message": "unknown user"
-    # }
+@app.route('/aba')
+def abba():
+    if request.method == 'GET': return render_template('hello.html')
+    email = request.args.get['email']
+    password = request.args.get['secret']
+    name = request.args.get['name']
+    address = request.args.get['address']
+    phone = request.args.get['phone']
+    qualification = request.args.get['qualification']
+    register = request.args.get['register']
+    if email is None or password is None:
+        abort(400)  # missing arguments
+    if User.query.filter_by(username=email).first() is not None:
+        abort(400)  # existing user
+
+    user = User(email, password, name, address, phone, qualification, register)
+    db_session.add(user)
+    db_session.commit()
+    return jsonify({'username': user.username}), 201, {'Location': url_for('get_user', id=user.id, _external=True)}
+
+
+@app.route('/sismic/login_form')
+def login():
+    username = request.args.get['username']
+    password = request.args.get['secret']
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        abort(400)
+    else:
+        if user.verify_password(password):
+            return jsonify({'username': user.username}), 201, {'Location': url_for('get_user', id=user.id, _external=True)}
+        else:
+            abort(400)
+
+
+@app.route('/sismic/reports', methods=["GET"])
+def show_reports():
+    pass
+
+
+@app.route('/sismic/upload_report', methods=["POST"])
+def upload_report():
+    pass
+
+
+@app.route('/sismic/upload_report_files', methods=["POST"])
+def upload_file():
+    pass
