9f98f32e33221c2e609225ca1e1540d794b24fe9
Matteo <matteo.liera@gmail.com>
2017-10-14 17:42:30 +0200

    upload e visualizzazione report (json oneline) funzionante

--- Database/models.py
+++ Database/models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, JSON
 from Database.database import Base
 from sqlalchemy import inspect
 
@@ -9,13 +9,13 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(50), unique=False)
     email = Column(String(120), unique=True)
-    secret = Column(String(128), unique=False)
+    secret = Column(String(64), unique=False)
     address = Column(String(120), unique=False)
     phone = Column(String(15), unique=False)
     qualification = Column(String(120), unique=False)
     registration = Column(String(120), unique=False)
     registration_date = Column(DateTime, unique=False)
-    confirmed = Column(Integer, unique=False)
+    confirmed = Column(Integer, unique=False)  # ?
 
     def toDict(self):
         return {c.key: getattr(self, c.key) for c in inspect(self).mapper.column_attrs}
@@ -36,11 +36,23 @@ class User(Base):
         return '<User %r>' % self.name
 
     def hash_password(self, password):
-        #obj = AES.new(self.name, AES.MODE_CBC, self.email)
-        #self.secret = obj.encrypt(password)
+        # obj = AES.new(self.name, AES.MODE_CBC, self.email)
+        # self.secret = obj.encrypt(password)
         return password
 
     def verify_password(self, password):
-        #obj = AES.new(self.name, AES.MODE_CBC, self.email)
-        #return password == obj.decrypt(self.secret)
+        # obj = AES.new(self.name, AES.MODE_CBC, self.email)
+        # return password == obj.decrypt(self.secret)
         return True
+
+
+class Report(Base):
+    __tablename__ = 'reports'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(String(120), ForeignKey("users.email"), nullable=False)
+    # depends on how "pretty" the json is (14k is for a normal report in one line)
+    data = Column(String(28000))
+
+    def __init__(self, data, user_id):
+        self.data = data
+        self.user_id = user_id
